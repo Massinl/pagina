500028bb64d96d09b92fe348e5a72a09b96c803b
massinl <cristian.massini@upr.edu>
2025-10-25 08:45:32 -0400

    css from pages now have their own folder

--- my-beats-site/src/pages/Beats.tsx
+++ my-beats-site/src/pages/Beats.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../lib/supabaseClient";
-import "./BeatsPage.css";
+import "./css/BeatsPage.css";
 
 type Beat = {
   id: number;
