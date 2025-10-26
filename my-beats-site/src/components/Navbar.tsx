a958a17d198699a5aa440f23a053a9fe9ceeea17
massinl <cristian.massini@upr.edu>
2025-10-25 22:04:07 -0400

    now the nav bar has its own css file

--- my-beats-site/src/components/Navbar.tsx
+++ my-beats-site/src/components/Navbar.tsx
@@ -1,45 +1,16 @@
 import { Link } from "react-router-dom";
+import './css/Navbar.css'; 
 
 export default function Navbar() {
   return (
-    <nav style={styles.nav}>
-      <h2 style={styles.logo}>🎧 El Kevin Beats</h2>
-      <div style={styles.links}>
-        <Link to="/" style={styles.link}>Home</Link>
-        <Link to="/beats" style={styles.link}>Beats</Link>
-        <Link to="/contact" style={styles.link}>Contact</Link>
+    <nav className="nav">
+      <h2 className="logo">🎧 KevBeats</h2>
+      <div className="links">
+        <Link to="/" className="link">Home</Link>
+        <Link to="/beats" className="link">Beats / Previews</Link>
+        <Link to="/contact" className="link">Contact</Link>
       </div>
     </nav>
   );
 }
 
-const styles = {
-  nav: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "1rem 3rem",
-    backgroundColor: "#121212",
-    boxShadow: "0 2px 10px rgba(0,0,0,0.5)",
-    position: "sticky",
-    top: 0,
-    zIndex: 100,
-  },
-  logo: {
-    color: "#fbfbfbff",
-    margin: 0,
-    fontWeight: "700",
-    fontSize: "1.5rem",
-  },
-  links: {
-    display: "flex",
-    gap: "2rem",
-  },
-  link: {
-  color: "rgba(255, 255, 255, 1)",
-  textDecoration: "none",
-  fontWeight: "500",
-  transition: "color 0.3s, text-shadow 0.3s",
-},
-
-};
